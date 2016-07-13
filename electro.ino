0cf710a8dcc38c998d156e9e67d685cf265b2c24
Gulden8a <oliver8agar@gmail.com>
2016-07-12 23:34:11 -0500

    Add files via upload

---
+++ electro.ino
@@ -0,0 +1,33 @@
+void setup() {
+
+  pinMode(0, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(1, OUTPUT);
+}
+void loop() {
+  int input=analogRead(A1);
+  int powr = map(input, 0, 1023, 1, 3);
+  if(powr==1)
+  {
+    digitalWrite(1,HIGH);
+    digitalWrite(3,LOW);
+    digitalWrite(4,LOW);
+  }
+  if(powr==2)
+  {
+    digitalWrite(1,HIGH);
+    digitalWrite(3,HIGH);
+    digitalWrite(4,LOW);
+  }
+  if(powr==3)
+  {
+    digitalWrite(1,HIGH);
+    digitalWrite(3,HIGH);
+    digitalWrite(4,HIGH);
+  }
+  digitalWrite(0, HIGH);   
+  delay(1/powr);              
+  digitalWrite(0, LOW);   
+  delay(1/powr);              
+}
